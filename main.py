a4405cde79b660531ac57d94cc007b517c1ed02d
Edwin Arias <ferwin.arias@gmail.com>
2026-05-02 11:51:05 -0500

    Se agregan las descripciones de rutinas y videos

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from utils.descripcion_rutinas import describir_datos_rutinas
+from utils.descripcion_videos import describir_datos_videos
 from utils.simulador_rutinas import generar_rutinas
 from utils.simulacion_rachas import generar_rachas
 from utils.simulacion_usuarios import generar_usuarios
 from utils.simulacion_videos import generar_videos
-from utils.descripcionusuario import describir_datos_usuario
+from utils.descripcion_usuario import describir_datos_usuario
 import pandas as pd
 
 usuarios = generar_usuarios(1000)
@@ -15,6 +17,7 @@ usuarios_ordenadas.to_csv("data/simulaciones_usuarios.csv")
 
 describir_datos_usuario(usuarios_ordenadas)
 
+
 videos = generar_videos(1000)
 
 videos_ordenados = pd.DataFrame(videos)
@@ -22,6 +25,8 @@ videos_ordenados = pd.DataFrame(videos)
 videos_ordenados.to_json("data/simulaciones_videos.json", orient = "records", indent=4)
 
 videos_ordenados.to_csv("data/simulaciones_videos.csv")
+
+describir_datos_videos(videos_ordenados)
 
 
 simulaciones=generar_rachas(1000)
@@ -40,3 +45,5 @@ rutinas_ordenadas = pd.DataFrame(rutinas)
 rutinas_ordenadas.to_json("data/simulaciones_rutinas.json", orient = "records", indent=4)
 
 rutinas_ordenadas.to_csv("data/simulaciones_rutinas.csv")
+
+describir_datos_rutinas(rutinas_ordenadas)
