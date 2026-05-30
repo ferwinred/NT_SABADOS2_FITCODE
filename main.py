81dabdf8d538eaa5d2eed821232b157d190cfbb1
Fernando Arias <fernando.arias@numrot.com>
2026-05-29 23:10:22 -0500

    Se ajustan graficos

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from notebook.transformar_rutinas  import transformar_rutinas
 from notebook.consumo_usuarios import consumo_usuarios
 from notebook.limpieza_usuarios import limpiar_datos_usuarios
 from notebook.transformar_usuarios import transformar_usuarios
-from notebook.graficacion_usuarios import graficar_barras
 from notebook.graficacion import graficar_lineas, graficar_barras, graficar_torta, graficar_mapa_calor
 
 rutinas = consumo_rutinas()
@@ -18,8 +17,17 @@ graficar_barras(
     "difficulty",
     "Duracion_total",
     "Grafico de duracion total por dificultad", 
-    "#FF5722",
+    ["#FF5722", "#2196F3", "#4CAF50"],
     "duracion_por_dificultad.png"
+)
+
+graficar_lineas(
+    agrupaciones["agrupacion2"],
+    "id",
+    "duracion_promedio",
+    "Grafico de duracion promedio de ejercicios", 
+    "#FF5722",
+    "duracion_promedio_lineas.png"
 )
 
 
@@ -28,11 +36,11 @@ data_frame_usuarios = pd.DataFrame(usuarios)
 data_frame_usuarios_limpio = limpiar_datos_usuarios(data_frame_usuarios)
 agrupaciones=transformar_usuarios(data_frame_usuarios_limpio)
 
-graficar_barras(
+graficar_torta(
     agrupaciones["agrupacion1"],
-    columna_categorias="sex",
-    columna_valores="conteo_por_sexo",
-    titulo="Cantidad de usuarios por sexo",
-    color_barras="#FF5733",
-    nombre_archivo="usuarios_por_sexo.png"
+    "sex",
+    "conteo_por_sexo",
+    "Cantidad de usuarios por sexo",
+     ["#FF5722", "#2196F3"],
+    "usuarios_por_sexo.png"
 )
